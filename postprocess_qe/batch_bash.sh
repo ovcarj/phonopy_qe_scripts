a68f852a3d342aa10274f6c3bfea256950498d0b
ovcarj <juraj.ovcar@gmail.com>
2024-04-12 12:47:51 +0200

    Use source instead of bash in batch_bash.sh

--- postprocess_qe/batch_bash.sh
+++ postprocess_qe/batch_bash.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+##!/bin/bash
 
 dirnames=$(ls -d batch_*/)
 bash_script_name=$1
@@ -6,6 +6,6 @@ bash_script_name=$1
 for dirname in $dirnames
 do
         cd $dirname
-        bash $submission_script_name
+        source $bash_script_name
         cd ..
 done
